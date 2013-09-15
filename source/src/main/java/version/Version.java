0ffd21d9c9bed915a8394397da8eb0f8c82b9343
Benoit DUMONT <bdumont@redoute.fr>
2013-09-15 20:56:56 +0200

    Updated version to 0.9.0

--- source/src/main/java/version/Version.java
+++ source/src/main/java/version/Version.java
@@ -1,8 +1,8 @@
 package version;
 public final class Version {
     public static final String PROJECT_NAME = "Cerberus";
-    public static final String VERSION = "1.0.0-SNAPSHOT";
-    public static final String PROJECT_NAME_VERSION = "Cerberus-1.0.0-SNAPSHOT";
+    public static final String VERSION = "0.9.0-SNAPSHOT";
+    public static final String PROJECT_NAME_VERSION = "Cerberus-0.9.0-SNAPSHOT";
     private Version() {
         //empty and commented construcotr to comply with Sonar rules
     }
