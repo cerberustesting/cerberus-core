d5556d5f690c3eca04ad4d1e96ff08390fe36dab
Benoit DUMONT <bdumont@redoute.fr>
2013-10-23 17:28:42 +0200

    Added environment parameter in GetNumberOfExecution Servlet. Also added an help message to explain the servlet parameters.

--- source/src/main/java/com/redcats/tst/servlet/publi/GetNumberOfExecutions.java
+++ source/src/main/java/com/redcats/tst/servlet/publi/GetNumberOfExecutions.java
@@ -60,12 +60,21 @@ public class GetNumberOfExecutions extends HttpServlet {
         IApplicationService MyapplicationService = appContext.getBean(ApplicationService.class);
 
         // Parsing all parameters.
+        String environment = ParameterParserUtil.parseStringParam(request.getParameter("environment"),"PROD");
         String test = ParameterParserUtil.parseStringParam(request.getParameter("test"),"");
         String application = ParameterParserUtil.parseStringParam(request.getParameter("application"),"");
         String country = ParameterParserUtil.parseStringParam(request.getParameter("country"),"");
         String controlStatus = ParameterParserUtil.parseStringParam(request.getParameter("controlstatus"),"");
         int NbMinutes = ParameterParserUtil.parseIntegerParam(request.getParameter("nbminuteshistory"),0);
 
+        String helpMessage="\nThis servlet return the number of execution that match the following criterias :\n"
+                + "nbminuteshistory [mandatory] : the number of minutes in the past from the moment the servlet is called. This parameter must be > 0. " + NbMinutes + "\n"
+                + "test : Executions done on the test. " + test + "\n"
+                + "environment : Environment where the execution happened. Default to PROD. " + environment + "\n"
+                + "country : Executions done on the country. " + country + "\n"
+                + "application : Executions done against that application. " + application + "\n"
+                + "controlstatus : execution that return the following status. " + controlStatus + "\n";
+        
         try {
 
             boolean error = false;
@@ -73,11 +82,13 @@ public class GetNumberOfExecutions extends HttpServlet {
             // Checking the parameter validity. nbminuteshistory is a mandatory parameter.
             if (NbMinutes == 0) {
                 out.println("Error - Parameter nbminuteshistory is mandatory. Please feed it in order to specify the elapsed time where the history should be considered.");
+                out.println(helpMessage);
                 error = true;
             }
             // Checking the parameter validity. If application has been entered, does it exist ?
             if (!application.equalsIgnoreCase("") && !MyapplicationService.isApplicationExist(application)) {
                 out.println("Error - Application does not exist  : " + application);
+                out.println(helpMessage);
                 error = true;
             }
 
@@ -92,7 +103,7 @@ public class GetNumberOfExecutions extends HttpServlet {
 
                 // Getting the lists of test cases the follow the criterias.
                 try {
-                    myList = MyTestExecutionService.findTCExecutionbyCriteria1(dateLimitFrom, test, "", application, country, "PROD", controlStatus, "WORKING");
+                    myList = MyTestExecutionService.findTCExecutionbyCriteria1(dateLimitFrom, test, "", application, country, environment, controlStatus, "WORKING");
                     out.println(myList.size());
                 } catch (CerberusException e) {
                     out.println("0");
