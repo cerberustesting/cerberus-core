fb104b390540266a084c97a02a426c9f0a0d0edc
Tiago Bernardes <tbernardes@redoute.pt>
2013-12-18 16:32:20 +0000

    Cerberus Version 0.9.1

--- source/src/main/java/org/cerberus/version/Version.java
+++ source/src/main/java/org/cerberus/version/Version.java
@@ -20,8 +20,8 @@
 package org.cerberus.version;
 public final class Version {
     public static final String PROJECT_NAME = "Cerberus";
-    public static final String VERSION = "0.9.1-SNAPSHOT";
-    public static final String PROJECT_NAME_VERSION = "Cerberus-0.9.1-SNAPSHOT";
+    public static final String VERSION = "0.9.1";
+    public static final String PROJECT_NAME_VERSION = "Cerberus-0.9.1";
     private Version() {
         //empty and commented construcotr to comply with Sonar rules
     }
