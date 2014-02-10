c6bdffa31dc34c6b3d41eb9dcf56ce0777a9f13b
memiks <memiks@memiks.fr>
2014-02-10 17:14:52 +0100

    [CRUD] Add functions in Java Class to be able to refactor code. #131
    Begin CRUD on TestDAO.java

--- source/src/main/java/org/cerberus/dao/impl/TestDAO.java
+++ source/src/main/java/org/cerberus/dao/impl/TestDAO.java
@@ -30,6 +30,7 @@ import org.cerberus.database.DatabaseSpring;
 import org.cerberus.entity.Test;
 import org.cerberus.factory.IFactoryTest;
 import org.cerberus.log.MyLogger;
+import org.cerberus.util.ParameterParserUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -62,25 +63,65 @@ public class TestDAO implements ITestDAO {
      */
     @Override
     public List<Test> findAllTest() {
-        List<Test> result = null;
-        final String query = "SELECT Test, Description, active, automated, tdatecrea FROM test";
+       
+        return findTestByCriteria(new Test());
+    }
 
+    public List<Test> findTestByCriteria(Test test) {
+        List<Test> result = null;
+        StringBuilder query = new StringBuilder("SELECT Test, Description, Active, Automated, TDateCrea FROM test ");
+
+        StringBuilder whereClause = new StringBuilder("WHERE 1=1 ");
+        
+        List<String> parameters = new ArrayList<String>();
+        
         Connection connection = this.databaseSpring.connect();
         try {
-            PreparedStatement preStat = connection.prepareStatement(query);
+            if(test.getTest() != null && !"".equals(test.getTest().trim())) {
+                whereClause.append("AND Test LIKE ? ");
+                parameters.add(test.getTest());
+            }
+
+            if(test.getDescription()!= null && !"".equals(test.getDescription().trim())) {
+                whereClause.append("AND Description LIKE ? ");
+                parameters.add(test.getDescription());
+            }
+
+            if(test.getActive()!= null && !"".equals(test.getActive().trim())) {
+                whereClause.append("AND Active LIKE ? ");
+                parameters.add(test.getActive());
+            }
+
+            if(test.getAutomated()!= null && !"".equals(test.getAutomated().trim())) {
+                whereClause.append("AND Automated LIKE ? ");
+                parameters.add(test.getAutomated());
+            }
+
+            if(test.gettDateCrea()!= null && !"".equals(test.gettDateCrea().trim())) {
+                whereClause.append("AND TDateCrea LIKE ? ");
+                parameters.add(test.gettDateCrea());
+            }
+            if(parameters.size() > 0) {
+                query.append(whereClause);
+            }
+
+            MyLogger.log(TestDAO.class.getName(), Level.ERROR, "Query : Test.findTestByCriteria : " + query.toString());
+            PreparedStatement preStat = connection.prepareStatement(query.toString());
+            if(parameters.size() > 0) {
+                int index = 0;
+                for (String parameter : parameters) {
+                    index++;
+                    preStat.setString(index, ParameterParserUtil.wildcardIfEmpty(parameter));
+                }
+            }
             try {
+                
+
                 ResultSet resultSet = preStat.executeQuery();
                 result = new ArrayList<Test>();
                 try {
                     while (resultSet.next()) {
-                        Test testToAdd = null;
-                        String test = resultSet.getString("test") == null ? "" : resultSet.getString("test");
-                        String description = resultSet.getString("description") == null ? "" : resultSet.getString("description");
-                        String active = resultSet.getString("active") == null ? "" : resultSet.getString("active");
-                        String automated = resultSet.getString("automated") == null ? "" : resultSet.getString("automated");
-//                        String tdatecrea = resultSet.getString("tdatecrea") == null ? "" : resultSet.getString("tdatecrea");
-                        testToAdd = factoryTest.create(test, description, active, automated, "");
-                        result.add(testToAdd);
+                        result.add(this.loadTestFromResultSet(resultSet));
                     }
                 } catch (SQLException exception) {
                     MyLogger.log(TestDAO.class.getName(), Level.ERROR, exception.toString());
@@ -100,9 +141,19 @@ public class TestDAO implements ITestDAO {
                     connection.close();
                 }
             } catch (SQLException e) {
-                MyLogger.log(TestCaseStepActionControlDAO.class.getName(), Level.WARN, e.toString());
+                MyLogger.log(TestDAO.class.getName(), Level.WARN, e.toString());
             }
         }
-        return result;
+        return result;    
+    }
+    
+    private Test loadTestFromResultSet(ResultSet resultSet) throws SQLException {
+        String test = resultSet.getString("Test");
+        String description = resultSet.getString("Description");
+        String active = resultSet.getString("Active");
+        String automated = resultSet.getString("Automated");
+        String tcactive = resultSet.getString("TDateCrea");
+
+        return factoryTest.create(test, description, active, automated, tcactive);
     }
 }
