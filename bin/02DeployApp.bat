ab59f69c8e88d62101b0c8bfe344254b88298d09
Benoit DUMONT <bdumont@redoute.fr>
2014-03-29 22:23:28 +0100

    Prepare deploy script for 0.9.1 version.

--- bin/02DeployApp.bat
+++ bin/02DeployApp.bat
@@ -10,4 +10,5 @@ rem ###### Script start here ######
 
 cd %MYPATH%
 CALL %GLASSFISHPATH%asadmin undeploy --target server --cascade=true Cerberus-0.9.0.war
-CALL %GLASSFISHPATH%asadmin deploy --target server --contextroot Cerberus --availabilityenabled=true %MYPATH%\..\Cerberus-0.9.0.war
+CALL %GLASSFISHPATH%asadmin undeploy --target server --cascade=true Cerberus-0.9.1.war
+CALL %GLASSFISHPATH%asadmin deploy --target server --contextroot Cerberus --availabilityenabled=true %MYPATH%\..\Cerberus-0.9.1.war
--- bin/02DeployApp.sh
+++ bin/02DeployApp.sh
@@ -10,5 +10,6 @@
 
 cd $MYPATH
 $GLASSFISHPATH/asadmin undeploy --target server --cascade=true Cerberus-0.9.0.war
-$GLASSFISHPATH/asadmin deploy --target server --contextroot Cerberus --availabilityenabled=true $MYPATH/../Cerberus-0.9.0.war
+$GLASSFISHPATH/asadmin undeploy --target server --cascade=true Cerberus-0.9.1.war
+$GLASSFISHPATH/asadmin deploy --target server --contextroot Cerberus --availabilityenabled=true $MYPATH/../Cerberus-0.9.1.war
 
