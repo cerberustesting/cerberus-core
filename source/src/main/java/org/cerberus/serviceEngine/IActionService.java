abf6394e22ade47b680b8e932923dc3fcb5fb462
bcivel <bcivel@redoute.fr>
2014-04-15 08:48:53 +0200

    Separate ActionService class from SeleniumService class #182 Licence Header

--- source/src/main/java/org/cerberus/serviceEngine/IActionService.java
+++ source/src/main/java/org/cerberus/serviceEngine/IActionService.java
@@ -1,9 +1,22 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Cerberus  Copyright (C) 2013  vertigo17
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This file is part of Cerberus.
+ *
+ * Cerberus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Cerberus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Cerberus.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package org.cerberus.serviceEngine;
 
 import org.cerberus.entity.TestCaseStepActionExecution;
