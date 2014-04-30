c86fec4ba398add61a1f1dc42ee371a5018e79db
Tiago Bernardes <tbernardes@redoute.pt>
2014-04-30 11:31:04 +0100

    Upload screenshot to TestCase, help users to explain the HowTo (#150)

--- source/src/main/java/org/cerberus/servlet/testCase/SavePicture.java
+++ source/src/main/java/org/cerberus/servlet/testCase/SavePicture.java
@@ -2,7 +2,6 @@ package org.cerberus.servlet.testCase;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
 import org.cerberus.entity.Parameter;
 import org.cerberus.entity.SavePictureConfig;
 import org.cerberus.exception.CerberusException;
@@ -19,7 +18,6 @@ import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
-import java.io.File;
 
 @WebServlet(name = "PictureConnector", urlPatterns = {"/PictureConnector"})
 public class SavePicture extends AbstractConnectorServlet {
@@ -29,33 +27,26 @@ public class SavePicture extends AbstractConnectorServlet {
     public static String HOME_SHARED_DOCS = "";
     public static String REALOBJECTURL = "virtualproxy";
 
-    private static Logger logger = Logger.getLogger(AbstractConnectorServlet.class);
-
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
-//        if(!StringUtils.isBlank(getServletContext().getInitParameter("HOME_SHARED_DOCS"))){
-            try {
-                ApplicationContext appContext = WebApplicationContextUtils.getWebApplicationContext(this.getServletContext());
-                IParameterService parameterService = appContext.getBean(ParameterService.class);
-                Parameter param = parameterService.findParameterByKey("cerberus_image_path", "");
-                HOME_SHARED_DOCS = param.getValue();
-//                logger.info("Parameter (cerberus_image_path) => "+param.getValue()+" | "+HOME_SHARED_DOCS);
-            } catch (CerberusException e) {
-                MyLogger.log(SavePicture.class.getName(), Level.FATAL, "Parameter (cerberus_image_path) not in Parameter table.");
-//                throw new ServletException(e.getMessage());
-            }
-//            File f=new File(HOME_SHARED_DOCS);
-//            if(!f.exists()){
-//                f.mkdirs();
-//            }
-//        }
-        if(!StringUtils.isBlank(getServletContext().getInitParameter("THUMBNAIL")))
+        try {
+            ApplicationContext appContext = WebApplicationContextUtils.getWebApplicationContext(this.getServletContext());
+            IParameterService parameterService = appContext.getBean(ParameterService.class);
+            Parameter param = parameterService.findParameterByKey("cerberus_image_path", "");
+            HOME_SHARED_DOCS = param.getValue();
+        } catch (CerberusException e) {
+            MyLogger.log(SavePicture.class.getName(), Level.FATAL, "Parameter (cerberus_image_path) not in Parameter table.");
+        }
+        if (!StringUtils.isBlank(getServletContext().getInitParameter("THUMBNAIL"))){
             THUMBNAIL = getServletContext().getInitParameter("THUMBNAIL");
-        if(!StringUtils.isBlank(getServletContext().getInitParameter("SHARED_DOCS")))
+        }
+        if (!StringUtils.isBlank(getServletContext().getInitParameter("SHARED_DOCS"))){
             SHARED_DOCS = getServletContext().getInitParameter("SHARED_DOCS");
-        if(!StringUtils.isBlank(getServletContext().getInitParameter("REALOBJECTURL")))
+        }
+        if (!StringUtils.isBlank(getServletContext().getInitParameter("REALOBJECTURL"))){
             REALOBJECTURL = getServletContext().getInitParameter("REALOBJECTURL");
+        }
     }
 
     @Override
