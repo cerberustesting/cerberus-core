e9b2a497929cb937e50fd92281ebe5a3ebd96e00
bcivel <bcivel@redoute.fr>
2014-07-17 22:06:58 +0200

    Add uninstall 9.0.0 version command in installation script #266

--- bin/02DeployApp.bat
+++ bin/02DeployApp.bat
@@ -9,6 +9,7 @@ CALL %CD%\00Config.bat
 rem ###### Script start here ######
 
 cd %MYPATH%
+CALL %GLASSFISHPATH%asadmin undeploy --target server --cascade=true Cerberus-0.9.0
 CALL %GLASSFISHPATH%asadmin undeploy --target server --cascade=true Cerberus-0.9.1
 CALL %GLASSFISHPATH%asadmin undeploy --target server --cascade=true Cerberus-1.0.0
 CALL %GLASSFISHPATH%asadmin deploy --target server --contextroot Cerberus --availabilityenabled=true %MYPATH%\..\Cerberus-1.0.0.war
