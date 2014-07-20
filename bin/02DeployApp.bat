5177ba871fd4552b5efbf127becd6a0f76e892c7
bcivel <bcivel@redoute.fr>
2014-07-20 20:43:45 +0200

    Version 1.0.1 instalation Package

--- bin/02DeployApp.bat
+++ bin/02DeployApp.bat
@@ -12,4 +12,5 @@ cd %MYPATH%
 CALL %GLASSFISHPATH%asadmin undeploy --target server --cascade=true Cerberus-0.9.0
 CALL %GLASSFISHPATH%asadmin undeploy --target server --cascade=true Cerberus-0.9.1
 CALL %GLASSFISHPATH%asadmin undeploy --target server --cascade=true Cerberus-1.0.0
-CALL %GLASSFISHPATH%asadmin deploy --target server --contextroot Cerberus --availabilityenabled=true %MYPATH%\..\Cerberus-1.0.0.war
+CALL %GLASSFISHPATH%asadmin undeploy --target server --cascade=true Cerberus-1.0.1
+CALL %GLASSFISHPATH%asadmin deploy --target server --contextroot Cerberus --availabilityenabled=true %MYPATH%\..\Cerberus-1.0.1.war
--- bin/02DeployApp.sh
+++ bin/02DeployApp.sh
@@ -12,5 +12,6 @@ cd $MYPATH
 $GLASSFISHPATH/asadmin undeploy --target server --cascade=true Cerberus-0.9.0
 $GLASSFISHPATH/asadmin undeploy --target server --cascade=true Cerberus-0.9.1
 $GLASSFISHPATH/asadmin undeploy --target server --cascade=true Cerberus-1.0.0
-$GLASSFISHPATH/asadmin deploy --target server --contextroot Cerberus --availabilityenabled=true $MYPATH/../Cerberus-1.0.0.war
+$GLASSFISHPATH/asadmin undeploy --target server --cascade=true Cerberus-1.0.1
+$GLASSFISHPATH/asadmin deploy --target server --contextroot Cerberus --availabilityenabled=true $MYPATH/../Cerberus-1.0.1.war
 
