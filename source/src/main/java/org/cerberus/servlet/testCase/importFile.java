91f578b8181fcca5eb30b0be8e9a73c1d7f16ece
bcivel <bcivel@redoute.fr>
2014-08-20 22:30:39 +0200

    [Refactoring] Clean Refactor Folder , remove JDBC in JSP, clean JSP #172

--- source/src/main/java/org/cerberus/servlet/testCase/importFile.java
+++ source/src/main/java/org/cerberus/servlet/testCase/importFile.java
@@ -17,7 +17,7 @@
  * You should have received a copy of the GNU General Public License
  * along with Cerberus.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.cerberus.refactor;
+package org.cerberus.servlet.testCase;
 
 import java.io.File;
 import java.io.IOException;
