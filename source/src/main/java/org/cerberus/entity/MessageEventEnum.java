ec7c1aac0b5703ffaef8a26970fd2fe202dcf0aa
bcivel <bcivel@redoute.fr>
2014-11-20 11:45:48 +0100

    wait action with time not working on CMP applications #460 closed #460

--- source/src/main/java/org/cerberus/entity/MessageEventEnum.java
+++ source/src/main/java/org/cerberus/entity/MessageEventEnum.java
@@ -119,6 +119,7 @@ public enum MessageEventEnum {
     ACTION_FAILED_MOUSEOVERANDWAIT_GENERIC(261, "FA", "Object is 'null'. This is mandatory in order to perform the action mouse over and wait.", true, true ,true , MessageGeneralEnum.EXECUTION_FA_ACTION),
     ACTION_FAILED_MOUSEOVERANDWAIT(262, "FA", "Mouse over '%ELEMENT%' but failed to wait '%TIME%' ms.", true, true ,true , MessageGeneralEnum.EXECUTION_FA_ACTION),
     ACTION_FAILED_WAIT(263, "FA", "Failed to wait '%TIME%' ms.", true, true ,true , MessageGeneralEnum.EXECUTION_FA_ACTION),
+    ACTION_FAILED_WAIT_INVALID_FORMAT(263, "FA", "Format of the timeout defined for this action is not numeric", true, true ,true , MessageGeneralEnum.EXECUTION_FA_ACTION),
     ACTION_FAILED_KEYPRESS(264, "FA", "Object and/or Property are ‘null’. Both are mandatory in order to perform the action type.", true, true ,true , MessageGeneralEnum.EXECUTION_FA_ACTION),
     ACTION_FAILED_OPENURL(265, "FA", "Failed to open '%URL%'.", true, true ,true , MessageGeneralEnum.EXECUTION_FA_ACTION),
     ACTION_FAILED_SELECT(266, "FA", "Object and/or Property are ‘null’. Both are mandatory in order to perform the action type.", true, true ,true , MessageGeneralEnum.EXECUTION_FA_ACTION),
