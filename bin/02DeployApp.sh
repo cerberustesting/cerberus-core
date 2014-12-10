a5050b2952dcdc346e1be65b74974f3a596a640c
Aurelien Bourdon <aurelien.bourdon@gmail.com>
2014-12-10 09:19:24 +0100

    Update deploy app files for the next release.

--- bin/02DeployApp.sh
+++ bin/02DeployApp.sh
@@ -13,5 +13,6 @@ $GLASSFISHPATH/asadmin undeploy --target server --cascade=true Cerberus-0.9.0
 $GLASSFISHPATH/asadmin undeploy --target server --cascade=true Cerberus-0.9.1
 $GLASSFISHPATH/asadmin undeploy --target server --cascade=true Cerberus-1.0.0
 $GLASSFISHPATH/asadmin undeploy --target server --cascade=true Cerberus-1.0.1
-$GLASSFISHPATH/asadmin deploy --target server --contextroot Cerberus --availabilityenabled=true $MYPATH/../Cerberus-1.0.1.war
+$GLASSFISHPATH/asadmin undeploy --target server --cascade=true Cerberus-1.0.2
+$GLASSFISHPATH/asadmin deploy --target server --contextroot Cerberus --availabilityenabled=true $MYPATH/../Cerberus-1.0.2.war
 
