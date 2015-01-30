4692ba360f161081226ba5c0851f83d630fea24b
bcivel <bcivel@redoute.fr>
2015-01-30 10:47:21 +0100

    Update deploy app files for the next release.

--- bin/02DeployApp.sh
+++ bin/02DeployApp.sh
@@ -16,5 +16,6 @@ $GLASSFISHPATH/asadmin undeploy --target server --cascade=true Cerberus-1.0.1
 $GLASSFISHPATH/asadmin undeploy --target server --cascade=true Cerberus-1.0.2
 $GLASSFISHPATH/asadmin undeploy --target server --cascade=true Cerberus-1.1.0
 $GLASSFISHPATH/asadmin undeploy --target server --cascade=true Cerberus-1.1.1
+$GLASSFISHPATH/asadmin undeploy --target server --cascade=true Cerberus-1.1.1
 $GLASSFISHPATH/asadmin deploy --target server --contextroot Cerberus --availabilityenabled=true $MYPATH/../Cerberus-1.1.1.war
 
