97919e1696d9ce88e7de77689a4b6a1a0ad4b92c
corentindms <cdescamps@redoute.fr>
2015-10-05 14:31:06 +0200

    #334 Added ReadRobot in the web.xml asynchronous call for tag report on homepage

--- source/src/main/webapp/js/pages/Homepage.js
+++ source/src/main/webapp/js/pages/Homepage.js
@@ -251,7 +251,7 @@ function loadTagExec() {
             type: "GET",
             url: "GetReportData",
             data: {CampaignName: "null", Tag: tagName},
-            async: false,
+            async: true,
             dataType: 'json',
             success: function (data) {
                 var tagData = {};
