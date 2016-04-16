aaf9a4a39c0e805745fd00546500f72b27ac1ba5
vertigo17 <vertigo17@free.fr>
2016-04-16 08:22:41 +0200

    Improved purge script with a lot better performance and specific purge on testcaseexecutionsysver table.

--- tools/SQLscripts/CerberusDatabaseScriptPurge.sql
+++ tools/SQLscripts/CerberusDatabaseScriptPurge.sql
@@ -37,38 +37,61 @@ DELETE FROM logevent
 -- SELECT count(*) FROM logevent
 where TO_DAYS(NOW()) - TO_DAYS(Time) >= 365 ;
 
+-- Further purge on DEPRECATED messages.
+DELETE FROM logevent
+-- select count(*) from logevent 
+where TO_DAYS(NOW()) - TO_DAYS(Time) >= 30 
+and Log like '[DEPRECATED]%' 
+order by LogEventID desc;
+
+
+
 -- EXECUTION Purges.
 -- -----------------
+
+SELECT @ID2 := IFNULL(MAX(ID), 0) from testcaseexecution where TO_DAYS(NOW()) - TO_DAYS(Start) >= 2 ;
+SELECT @ID60 := IFNULL(MAX(ID), 0) from testcaseexecution where TO_DAYS(NOW()) - TO_DAYS(Start) >= 60 ;
+SELECT @ID100 := IFNULL(MAX(ID), 0) from testcaseexecution where TO_DAYS(NOW()) - TO_DAYS(Start) >= 100 ;
+SELECT @ID200 := IFNULL(MAX(ID), 0) from testcaseexecution where TO_DAYS(NOW()) - TO_DAYS(Start) >= 200 ;
+SELECT @ID400 := IFNULL(MAX(ID), 0) from testcaseexecution where TO_DAYS(NOW()) - TO_DAYS(Start) >= 400 ;
+SELECT @ID730 := IFNULL(MAX(ID), 0) from testcaseexecution where TO_DAYS(NOW()) - TO_DAYS(Start) >= 730 ;
+
 
 -- Test Execution Control History
 DELETE FROM testcasestepactioncontrolexecution
 -- SELECT count(*) FROM testcasestepactioncontrolexecution
-where TO_DAYS(NOW()) - TO_DAYS(Start) >= 200 ;
+where ID < @ID200 ;
 
 -- Test Execution Action History
 DELETE FROM testcasestepactionexecution
 -- SELECT count(*) FROM testcasestepactionexecution
-where TO_DAYS(NOW()) - TO_DAYS(Start) >= 200 ;
+where ID < @ID200 ;
 
 -- Test Execution Step History
 DELETE FROM testcasestepexecution
 -- SELECT count(*) FROM testcasestepexecution
-where TO_DAYS(NOW()) - TO_DAYS(Start) >= 200 ;
+where ID < @ID200 ;
 
 -- Test Execution Property History
 DELETE FROM testcaseexecutiondata
 -- SELECT count(*) FROM testcaseexecutiondata
-where TO_DAYS(NOW()) - TO_DAYS(Start) >= 200 ;
+where ID < @ID200 ;
 
 -- Test Execution www det
 DELETE FROM testcaseexecutionwwwdet
 -- SELECT count(*) FROM testcaseexecutionwwwdet
-where TO_DAYS(NOW()) - TO_DAYS(Start) >= 60 ;
+where ID < @ID60 ;
 
 -- Test Execution History
 DELETE FROM testcaseexecution
 -- SELECT count(*) FROM testcaseexecution
-where TO_DAYS(NOW()) - TO_DAYS(Start) >= 730 ;
+where ID < @ID730 ;
+
+-- Test Execution System versionning History
+DELETE FROM testcaseexecutionsysver
+-- SELECT count(*) FROM testcaseexecutionsysver
+where ID < @ID200 ;
+
 
 
 -- EXECUTION Purges (BAM Test Cases).
@@ -77,27 +100,28 @@ where TO_DAYS(NOW()) - TO_DAYS(Start) >= 730 ;
 -- Test Execution Control History
 DELETE FROM testcasestepactioncontrolexecution
 -- SELECT count(*) FROM testcasestepactioncontrolexecution
-where TO_DAYS(NOW()) - TO_DAYS(Start) >= 100 and test = 'Business Activity Monitor';
+where ID < @ID100 and test = 'Business Activity Monitor';
 
 -- Test Execution Action History
 DELETE FROM testcasestepactionexecution
 -- SELECT count(*) FROM testcasestepactionexecution
-where TO_DAYS(NOW()) - TO_DAYS(Start) >= 100 and test = 'Business Activity Monitor';
+where ID < @ID100 and test = 'Business Activity Monitor';
 
 -- Test Execution Step History
 DELETE FROM testcasestepexecution
 -- SELECT count(*) FROM testcasestepexecution
-where TO_DAYS(NOW()) - TO_DAYS(Start) >= 100 and test = 'Business Activity Monitor';
+where ID < @ID100 and test = 'Business Activity Monitor';
 
 -- Test Execution History
 DELETE FROM testcaseexecution
 -- SELECT count(*) FROM testcaseexecution
-where  TO_DAYS(NOW()) - TO_DAYS(Start) >= 400 and test = 'Business Activity Monitor';
+where  ID < @ID400 and test = 'Business Activity Monitor';
 
 -- Test Execution History for BAM tests that reported no result.
 DELETE FROM testcaseexecution
 -- SELECT count(*) FROM testcaseexecution
-where  TO_DAYS(NOW()) - TO_DAYS(Start) >= 2 and test = 'Business Activity Monitor' and ControlStatus in ('NA','FA','KO','PE','CA');
+where  ID < @ID2 and test = 'Business Activity Monitor' and ControlStatus in ('NA','FA','KO','PE','CA');
+
 
 -- EXECUTION Purges (Performance Monitor Test Cases).
 -- -----------------------
@@ -105,20 +129,20 @@ where  TO_DAYS(NOW()) - TO_DAYS(Start) >= 2 and test = 'Business Activity Monito
 -- Test Execution Control History
 DELETE FROM testcasestepactioncontrolexecution
 -- SELECT count(*) FROM testcasestepactioncontrolexecution
-where TO_DAYS(NOW()) - TO_DAYS(Start) >= 100 and test = 'Performance Monitor';
+where ID < @ID100 and test = 'Performance Monitor';
 
 -- Test Execution Action History
 DELETE FROM testcasestepactionexecution
 -- SELECT count(*) FROM testcasestepactionexecution
-where TO_DAYS(NOW()) - TO_DAYS(Start) >= 100 and test = 'Performance Monitor';
+where ID < @ID100 and test = 'Performance Monitor';
 
 -- Test Execution Step History
 DELETE FROM testcasestepexecution
 -- SELECT count(*) FROM testcasestepexecution
-where TO_DAYS(NOW()) - TO_DAYS(Start) >= 100 and test = 'Performance Monitor';
+where ID < @ID100 and test = 'Performance Monitor';
 
 -- Test Execution History for BAM tests.
 DELETE FROM testcaseexecution
 -- SELECT count(*) FROM testcaseexecution
-where  TO_DAYS(NOW()) - TO_DAYS(Start) >= 200 and test = 'Performance Monitor';
+where  ID < @ID200 and test = 'Performance Monitor';
 
