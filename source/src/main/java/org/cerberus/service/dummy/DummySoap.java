2ec4a8196eac7c0349f2409d7b16a1547009fc4e
bcivel <bcivel@redoute.fr>
2016-05-19 22:17:25 +0200

    #740 Added Dummy Soap webservice for testing purpose

---
+++ source/src/main/java/org/cerberus/service/dummy/DummySoap.java
@@ -0,0 +1,41 @@
+/*
+ * Cerberus  Copyright (C) 2016  vertigo17
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This file is part of Cerberus.
+ *
+ * Cerberus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Cerberus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Cerberus.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.cerberus.service.dummy;
+
+import javax.jws.WebParam;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.Style;
+
+/**
+ *
+ * @author bcivel
+ */
+@WebService
+@SOAPBinding(style = Style.RPC)
+public class DummySoap {
+
+    public String checkSoapResponse(
+            @WebParam(name = "param1") String stringParam1,
+            @WebParam(name = "param2") Integer integerParam2) {
+
+        return "Success";
+    }
+}
