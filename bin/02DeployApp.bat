eea7ebce0a0c8dc85c77f219cd3e99bf82ea1256
bcivel <bcivel@redoute.fr>
2016-06-29 21:59:55 +0100

    Update deploy app files for the next release.

--- bin/02DeployApp.bat
+++ bin/02DeployApp.bat
@@ -20,4 +20,5 @@ CALL %GLASSFISHPATH%asadmin undeploy --target server --cascade=true Cerberus-1.1
 CALL %GLASSFISHPATH%asadmin undeploy --target server --cascade=true Cerberus-1.1.2
 CALL %GLASSFISHPATH%asadmin undeploy --target server --cascade=true Cerberus-1.1.3
 CALL %GLASSFISHPATH%asadmin undeploy --target server --cascade=true Cerberus-1.1.4
-CALL %GLASSFISHPATH%asadmin deploy --target server --contextroot Cerberus --availabilityenabled=true %MYPATH%\..\Cerberus-1.1.4.war
+CALL %GLASSFISHPATH%asadmin undeploy --target server --cascade=true Cerberus-1.1.5
+CALL %GLASSFISHPATH%asadmin deploy --target server --contextroot Cerberus --availabilityenabled=true %MYPATH%\..\Cerberus-1.1.5.war
--- bin/02DeployApp.sh
+++ bin/02DeployApp.sh
@@ -20,5 +20,6 @@ $GLASSFISHPATH/asadmin undeploy --target server --cascade=true Cerberus-1.1.1
 $GLASSFISHPATH/asadmin undeploy --target server --cascade=true Cerberus-1.1.2
 $GLASSFISHPATH/asadmin undeploy --target server --cascade=true Cerberus-1.1.3
 $GLASSFISHPATH/asadmin undeploy --target server --cascade=true Cerberus-1.1.4
-$GLASSFISHPATH/asadmin deploy --target server --contextroot Cerberus --availabilityenabled=true $MYPATH/../Cerberus-1.1.4.war
+$GLASSFISHPATH/asadmin undeploy --target server --cascade=true Cerberus-1.1.5
+$GLASSFISHPATH/asadmin deploy --target server --contextroot Cerberus --availabilityenabled=true $MYPATH/../Cerberus-1.1.5.war
 
