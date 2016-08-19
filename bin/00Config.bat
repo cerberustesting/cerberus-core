87c67a1b430c0e94782eaaad9b09163691147901
Corentin <corentinvanson@hotmail.com>
2016-08-19 17:23:07 +0200

    remove unused file

--- bin/00Config.bat
+++ bin/00Config.bat
@@ -10,5 +10,5 @@ SET DTBSRVPORT=3306
 SET DTBNAME=cerberus
 SET DTBUSER=cerberus
 SET DTBPASSWD=toto
-SET GLASSFISHPATH="C:\glassfish4\glassfish\bin"
+SET GLASSFISHPATH="D:\glassfish4\glassfish\bin\"
 
