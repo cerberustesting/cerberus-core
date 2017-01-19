e1ae596c43ebb38a0d77e11762423ed3ecc6eb36
Benoit CIVEL <bcivel@redoute.fr>
2017-01-19 18:06:40 +0100

    fix(#1122) : Steps block load correctly now#1122

--- source/src/main/webapp/js/pages/ExecutionDetail.js
+++ source/src/main/webapp/js/pages/ExecutionDetail.js
@@ -76,24 +76,24 @@ function initPage(id) {
 
     var doc = new Doc();
     var height = $("nav.navbar.navbar-inverse.navbar-static-top").outerHeight(true) + $("div.alert.alert-warning").outerHeight(true) + $(".page-title-line").outerHeight(true) - 10;
-    $('#executionHeader').affix({offset: {top: height}});
+    //$('#executionHeader').affix({offset: {top: height}});
 
     var wrap = $(window);
 
-    wrap.on("scroll", function (e) {
-        if ($("#executionHeader").width() != $("#executionHeader").parent().width() - 30) {
-            $("#executionHeader").width($("#executionHeader").parent().width() - 30);
-            $("#list-wrapper").width($("#nav-execution").width());
-        }
-    });
-
-    wrap.resize(function (e) {
-        if ($("#executionHeader").width() != $("#executionHeader").parent().width() - 30) {
-            $("#executionHeader").width($("#executionHeader").parent().width() - 30);
-            $("#list-wrapper").width($("#nav-execution").width());
-        }
-        $('.action [data-toggle="tooltip"], .control [data-toggle="tooltip"]').tooltip('show');
-    })
+//    wrap.on("scroll", function (e) {
+//        if ($("#executionHeader").width() != $("#executionHeader").parent().width() - 30) {
+//            $("#executionHeader").width($("#executionHeader").parent().width() - 30);
+//            $("#list-wrapper").width($("#nav-execution").width());
+//        }
+//    });
+//
+//    wrap.resize(function (e) {
+//        if ($("#executionHeader").width() != $("#executionHeader").parent().width() - 30) {
+//            $("#executionHeader").width($("#executionHeader").parent().width() - 30);
+//            $("#list-wrapper").width($("#nav-execution").width());
+//        }
+//        $('.action [data-toggle="tooltip"], .control [data-toggle="tooltip"]').tooltip('show');
+//    })
 
     $("#editTcInfo").prop("disabled", true);
     $("#runTestCase").prop("disabled", true);
@@ -134,17 +134,17 @@ function initPage(id) {
 
     var wrap = $(window);
 
-    wrap.on("scroll", function (e) {
-        if ($("#list-wrapper").width() != $("#nav-execution").parent().width() - 30) {
-            $("#list-wrapper").width($("#nav-execution").width());
-        }
-    });
-
-    wrap.resize(function (e) {
-        if ($("#list-wrapper").width() != $("#nav-execution").parent().width() - 30) {
-            $("#list-wrapper").width($("#nav-execution").width());
-        }
-    })
+//    wrap.on("scroll", function (e) {
+//        if ($("#list-wrapper").width() != $("#nav-execution").parent().width() - 30) {
+//            $("#list-wrapper").width($("#nav-execution").width());
+//        }
+//    });
+//
+//    wrap.resize(function (e) {
+//        if ($("#list-wrapper").width() != $("#nav-execution").parent().width() - 30) {
+//            $("#list-wrapper").width($("#nav-execution").width());
+//        }
+//    })
 }
 
 function displayPageLabel(doc) {
