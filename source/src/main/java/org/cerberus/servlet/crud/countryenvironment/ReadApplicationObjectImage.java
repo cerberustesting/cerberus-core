07717acc560440ee749a7a413203ed32d3e6d664
Benoit CIVEL <bcivel@redoute.fr>
2017-04-26 22:06:09 +0200

    fix(#1288) : Removed ReadApplicationObjectImage from zzpublic.

--- source/src/main/java/org/cerberus/servlet/crud/countryenvironment/ReadApplicationObjectImage.java
+++ source/src/main/java/org/cerberus/servlet/crud/countryenvironment/ReadApplicationObjectImage.java
@@ -17,7 +17,7 @@
  * You should have received a copy of the GNU General Public License
  * along with Cerberus.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.cerberus.servlet.zzpublic;
+package org.cerberus.servlet.crud.countryenvironment;
 
 import com.mortennobel.imagescaling.DimensionConstrain;
 import com.mortennobel.imagescaling.ResampleOp;
