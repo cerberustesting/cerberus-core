5030ece178ab06711dd230ed82f36f7e2f37f07e
nicodeur <nico.deblock@gmail.com>
2018-02-07 19:51:04 +0100

    chore: prepare the new 3.1.0 version

--- bin/02DeployApp.bat
+++ bin/02DeployApp.bat
@@ -35,4 +35,5 @@ CALL %GLASSFISHPATH%asadmin.bat undeploy --target server --cascade=true Cerberus
 CALL %GLASSFISHPATH%asadmin.bat undeploy --target server --cascade=true Cerberus-2.0.0
 CALL %GLASSFISHPATH%asadmin.bat undeploy --target server --cascade=true Cerberus-3.0.0
 CALL %GLASSFISHPATH%asadmin.bat undeploy --target server --cascade=true Cerberus-3.1.0
+CALL %GLASSFISHPATH%asadmin.bat undeploy --target server --cascade=true Cerberus-3.1.0
 CALL %GLASSFISHPATH%asadmin.bat deploy --target server --contextroot Cerberus --availabilityenabled=true %MYPATH%\..\source\target\Cerberus-3.1.0.war
