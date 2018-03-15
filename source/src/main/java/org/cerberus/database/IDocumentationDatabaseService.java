f8ead35df1d764b6329217fc73e11dec46e6516f
ryltar <mart1.guillaume@free.fr>
2018-03-15 11:38:20 +0100

    #1645

---
+++ source/src/main/java/org/cerberus/database/IDocumentationDatabaseService.java
@@ -0,0 +1,48 @@
+/**
+ * Cerberus Copyright (C) 2013 - 2017 cerberustesting
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This file is part of Cerberus.
+ *
+ * Cerberus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Cerberus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Cerberus.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.cerberus.database;
+
+import java.util.ArrayList;
+
+/**
+ * @author ryltar
+ */
+public interface IDocumentationDatabaseService {
+
+    /**
+     * @param SQLString String that contains the SQL that will be executed
+     *                  against the Cerberus database.
+     * @return "OK" if the SQL executed correctly and a string with the error
+     *         when not executed.
+     */
+    String exeSQL(String SQLString);
+    
+    /**
+     * This method call exeSQL with documentation script
+     * right after DocumentationDatabaseService singleton is created
+     */
+    void init();
+
+    /**
+     * @return an array of string that contain all the SQL instructions to refresh
+     * documentation table
+     */
+    ArrayList<String> getSqlDocumentation();
+}
