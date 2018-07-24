6af0e73644c13b0c4d2487d48ae61c19aff16780
ryltar <mart1.guillaume@free.fr>
2018-07-24 09:25:01 +0200

    chore:source prepare the new 3.7.1 version and update database version to 1359

--- bin/02DeployApp.sh
+++ bin/02DeployApp.sh
@@ -42,4 +42,5 @@ $GLASSFISHPATH/asadmin undeploy --target server --cascade=true Cerberus-3.4
 $GLASSFISHPATH/asadmin undeploy --target server --cascade=true Cerberus-3.5
 $GLASSFISHPATH/asadmin undeploy --target server --cascade=true Cerberus-3.6
 $GLASSFISHPATH/asadmin undeploy --target server --cascade=true Cerberus-3.7
-$GLASSFISHPATH/asadmin deploy --target server --contextroot Cerberus --availabilityenabled=true $MYPATH/../Cerberus-3.7.war
+$GLASSFISHPATH/asadmin undeploy --target server --cascade=true Cerberus-3.7.1
+$GLASSFISHPATH/asadmin deploy --target server --contextroot Cerberus --availabilityenabled=true $MYPATH/../Cerberus-3.7.1.war
