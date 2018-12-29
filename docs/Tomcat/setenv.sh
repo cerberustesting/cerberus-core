fb19fcc9802d863b29cffa056972d56c4ac54106
vertigo17 <vertigo17@free.fr>
2018-12-29 15:47:37 +0100

    Tomcat sample config files.

---
+++ docs/Tomcat/setenv.sh
@@ -0,0 +1,2 @@
+CATALINA_OPTS="-Dorg.cerberus.environment=prd -Dorg.cerberus.authentification=none -Dorg.cerberus.keycloak.realm=Cerberus -Dorg.cerberus.keycloak.client=cerberus -Dorg.cerberus.keycloak.url=http://localhost:8080/auth -Xmx1024m"
+
