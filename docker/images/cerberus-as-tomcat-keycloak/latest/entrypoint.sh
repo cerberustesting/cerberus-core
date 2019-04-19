32fc9e0e971c01c7f19eedcdb8dad45a78cfd3e0
Nicolas Deblock <nico.deblock@gmail.com>
2019-04-19 17:51:12 +0200

    docker with tomcat server

--- docker/images/cerberus-as-tomcat-keycloak/latest/entrypoint.sh
+++ docker/images/cerberus-as-tomcat-keycloak/latest/entrypoint.sh
@@ -28,5 +28,16 @@
 export CATALINA_OPTS="$CATALINA_OPTS -DDATABASE_HOST=$DATABASE_HOST -DDATABASE_PORT=$DATABASE_PORT -DDATABASE_NAME=$DATABASE_NAME -DDATABASE_USER=$DATABASE_USER -DDATABASE_PASSWORD=$DATABASE_PASSWORD"
 
 /usr/local/tomcat/bin/catalina.sh start
+#sh /keylock/${KEYCLOACK_NAME}/bin/standalone.sh -b 0.0.0.0 > /var/log/keylock.log &
+#bash /keylock/${KEYCLOACK_NAME}/bin/add-user-keycloak.sh -u cerberus -p cerberus
+#/keylock/keycloak-4.8.0.Final/bin/jboss-cli.sh --connect command=:reload
+
+#bash /keylock/${KEYCLOACK_NAME}/bin/kcadm.sh config credentials --server http://localhost:8080/auth --realm Cerberus --user cerberus --password cerberus --client cerberus
+#bash /keylock/${KEYCLOACK_NAME}/bin/kcadm.sh create realms -s --server http://localhost:8080/auth realm=Cerberus -s enabled=true -o --server http://localhost:8080/auth --realm Cerberus --user cerberus --password cerberus
+
+#CID=$(bash /keylock/${KEYCLOACK_NAME}/bin/kcadm.sh create clients -r Cerberus -s clientId=cerberus -s 'redirectUris=["http://localhost:8180/Cerberus/*"]' -i)
+#bash /keylock/${KEYCLOACK_NAME}/bin/kcadm.sh get clients/$CID/installation/providers/keycloak-oidc-keycloak-json
+
+#tail -F /var/log/keylock.log /usr/local/tomcat/logs/catalina.out
 
 tail -F /usr/local/tomcat/logs/catalina.out
