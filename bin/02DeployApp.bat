0f405e1a47dabf1d22777b4f6eeb24c5ee8f90b9
Benoit DUMONT <vertigo17@free.fr>
2019-07-28 11:53:19 +0200

    chore:source prepare the new 4.1 version and update database version to 1419

--- bin/02DeployApp.bat
+++ bin/02DeployApp.bat
@@ -48,4 +48,5 @@ CALL %GLASSFISHPATH%asadmin.bat undeploy --target server --cascade=true Cerberus
 CALL %GLASSFISHPATH%asadmin.bat undeploy --target server --cascade=true Cerberus-3.10
 CALL %GLASSFISHPATH%asadmin.bat undeploy --target server --cascade=true Cerberus-3.11
 CALL %GLASSFISHPATH%asadmin.bat undeploy --target server --cascade=true Cerberus-4.0
-CALL %GLASSFISHPATH%asadmin.bat deploy --target server --contextroot Cerberus --availabilityenabled=true %MYPATH%\..\Cerberus-4.0.war
+CALL %GLASSFISHPATH%asadmin.bat undeploy --target server --cascade=true Cerberus-4.1
+CALL %GLASSFISHPATH%asadmin.bat deploy --target server --contextroot Cerberus --availabilityenabled=true %MYPATH%\..\Cerberus-4.1.war
