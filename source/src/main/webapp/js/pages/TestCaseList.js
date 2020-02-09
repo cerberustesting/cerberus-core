43d3720ea5677319b28888fc537c5438e6e22090
Benoit DUMONT <vertigo17@free.fr>
2020-02-09 22:08:47 +0100

    Country columns on testcase list page for #2091 and #2015

--- source/src/main/webapp/js/pages/TestCaseList.js
+++ source/src/main/webapp/js/pages/TestCaseList.js
@@ -50,12 +50,12 @@ function initPage() {
         'placement': 'auto',
         'container': 'body'}
     );
-    
+
     var availableUsers = getUserArray(true);
     $("input#massExecutor").autocomplete({
         source: availableUsers
     });
-    
+
 }
 
 function initMassActionModal() {
@@ -988,13 +988,13 @@ function aoColumnsFunc(countries, tableId) {
                 var dataTitle = meta.settings.aoColumns[meta.col].sTitle;
 
                 if (row.hasPermissionsUpdate) {
-                    if (row.hasOwnProperty("countryList") && row["countryList"].hasOwnProperty(dataTitle)) {
+                    if (row.hasOwnProperty("countryList") && row["countryList"].some(item => item.country === dataTitle)) {
                         return '<input type="checkbox" name="' + dataTitle + '" data-test="' + row.test + '" data-testcase="' + row.testCase + '" onchange="setCountry(this);" checked/>';
                     } else {
                         return '<input type="checkbox" name="' + dataTitle + '" data-test="' + row.test + '" data-testcase="' + row.testCase + '" onchange="setCountry(this);"/>';
                     }
                 } else {
-                    if (row.hasOwnProperty("countryList") && row["countryList"].hasOwnProperty(dataTitle)) {
+                    if (row.hasOwnProperty("countryList") && row["countryList"].some(item => item.country === dataTitle)) {
                         return '<input type="checkbox" checked disabled/>';
                     } else {
                         return '<input type="checkbox" disabled/>';
