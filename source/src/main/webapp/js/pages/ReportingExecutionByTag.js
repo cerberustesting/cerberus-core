b28098364ae4eaa4a9ef077dda0ce582df257e26
Praddox <praddox@gmail.com>
2020-11-09 11:01:36 +0100

    [Report] Add select/unselect all testcase + add color

--- source/src/main/webapp/js/pages/ReportingExecutionByTag.js
+++ source/src/main/webapp/js/pages/ReportingExecutionByTag.js
@@ -1261,14 +1261,14 @@ function renderOptionsForExeList(selectTag, fullListSelected) {
         contentToAdd += "<label class='checkbox-inline fontKO'><input id='selectAllQueueKO' type='checkbox'></input>KO</label>";
         contentToAdd += "<label class='checkbox-inline fontFA'><input id='selectAllQueueFAManual' type='checkbox'></input>FA (Manual)</label>";
         contentToAdd += "<label class='checkbox-inline fontKO'><input id='selectAllQueueKOManual' type='checkbox'></input>KO (Manual)</label>";
-        contentToAdd += "<label class='checkbox-inline marginRight10 fontNA'><input id='selectAllQueueNA' type='checkbox'></input>NA</label>";
+        contentToAdd += "<label class='checkbox-inline fontNA'><input id='selectAllQueueNA' type='checkbox'></input>NA</label>"; 
+        contentToAdd += "<label class='checkbox-inline marginRight10'><input id='fullList' type='checkbox' " + fullListSelected + "></input>Full List</label>";       
         contentToAdd += "<div class='btn-group marginRight20 marginLeft20'>";
         contentToAdd += "<button id='submitExe' type='button' disabled='disabled' title='Submit again the selected executions.' class='btn btn-default'><span class='glyphicon glyphicon-play'></span> Submit Again</button>";
         contentToAdd += "<button id='btnGroupDrop4' type='button' class='btn btn-default dropdown-toggle' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'><span class='caret'></span><span class='sr-only'>Toggle Dropdown</span></button>";
         contentToAdd += "<div class='dropdown-menu'><button id='submitExewithDep' type='button' disabled='disabled' title='Submit again the selected executions with all dependencies.' class='btn btn-default'><span class='glyphicon glyphicon-play'></span> Submit Again with Dep</button></div>";
         contentToAdd += "</div>";
         contentToAdd += "<a href='TestCaseExecutionQueueList.jsp?tag=" + selectTag + "'><button id='openqueue' type='button' class='btn btn-default marginLeft20'><span class='glyphicon glyphicon-list'></span> Open Queue</button></a>";
-     //   contentToAdd += "<label class='checkbox-inline'><input id='fullList' type='checkbox' " + fullListSelected + "></input>Full List</label>";
         contentToAdd += "<button id='refresh' type='button' title='Refresh.' class='btn btn-default marginLeft20' onclick='loadAllReports()'><span class='glyphicon glyphicon-refresh'></span> Refresh</button>";
         contentToAdd += "</div>";
       
@@ -1298,7 +1298,15 @@ function renderOptionsForExeList(selectTag, fullListSelected) {
         });
         
         $("#selectAllStatus").on("click", function () {
-            $("#statusFilterList input").prop('checked', true);
+        	$("#selectAllQueueOK").prop('checked', true);
+        	$("#selectAllQueueFA").prop('checked', true);
+        	$("#selectAllQueueKO").prop('checked', true);
+        	$("#selectAllQueueFAManual").prop('checked', true);
+        	$("#selectAllQueueKOManual").prop('checked', true);
+        	$("#selectAllQueueNA").prop('checked', true);
+        	$("#selectAllQueueQEERROR").prop('checked', true);
+            $("#fullList").prop('checked', false);
+        	
             selectAllQueue("selectAllQueueOK", "N", "OK");
             selectAllQueue("selectAllQueueFA", "N", "FA");
             selectAllQueue("selectAllQueueKO", "N", "KO");
