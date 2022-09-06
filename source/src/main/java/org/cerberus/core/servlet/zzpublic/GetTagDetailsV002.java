410bbfc700d296376797825a0f1dbac04a3e8e95
MorganLmd <morgan.lombard@protonmail.com>
2022-09-06 11:11:21 +0200

    #2365 servlet cleaning

--- source/src/main/java/org/cerberus/core/servlet/zzpublic/GetTagDetailsV002.java
+++ source/src/main/java/org/cerberus/core/servlet/zzpublic/GetTagDetailsV002.java
@@ -19,39 +19,34 @@
  */
 package org.cerberus.core.servlet.zzpublic;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.cerberus.core.crud.entity.Invariant;
+import org.cerberus.core.crud.entity.Tag;
+import org.cerberus.core.crud.entity.TestCaseExecution;
+import org.cerberus.core.crud.service.IInvariantService;
+import org.cerberus.core.crud.service.ILogEventService;
+import org.cerberus.core.crud.service.IParameterService;
+import org.cerberus.core.crud.service.ITagService;
+import org.cerberus.core.crud.service.ITestCaseExecutionService;
+import org.cerberus.core.crud.service.impl.LogEventService;
+import org.cerberus.core.exception.CerberusException;
+import org.cerberus.core.service.authentification.IAPIKeyService;
+import org.cerberus.core.util.ParameterParserUtil;
+import org.cerberus.core.util.StringUtil;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.cerberus.core.exception.CerberusException;
-import org.cerberus.core.crud.entity.Invariant;
-import org.cerberus.core.crud.entity.Tag;
-import org.cerberus.core.crud.entity.TestCaseExecution;
-import org.cerberus.core.crud.service.IInvariantService;
-import org.cerberus.core.crud.service.ILogEventService;
-import org.cerberus.core.crud.service.ITagService;
-import org.cerberus.core.crud.service.IParameterService;
-import org.cerberus.core.crud.service.ITestCaseExecutionService;
-import org.cerberus.core.crud.service.impl.LogEventService;
-import org.cerberus.core.service.authentification.IAPIKeyService;
-import org.cerberus.core.util.ParameterParserUtil;
+import java.io.IOException;
 import java.text.ParseException;
-import org.json.JSONObject;
-import org.springframework.context.ApplicationContext;
-import org.springframework.web.context.support.WebApplicationContextUtils;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.cerberus.core.util.StringUtil;
+import java.util.List;
 
 /**
- *
  * @author Nouxx
  * @author vertigo17
  */
@@ -63,12 +58,7 @@ public class GetTagDetailsV002 extends HttpServlet {
     private ITagService tagService;
     private IParameterService parameterService;
     private IInvariantService invariantService;
-
-    private List<Invariant> prioritiesList = new ArrayList<>();
-    private List<Invariant> countriesList = new ArrayList<>();
-    private List<Invariant> environmentsList = new ArrayList<>();
-
-    private String cerberusUrlParameter;
+    private ILogEventService logEventService;
 
     private static final Logger LOG = LogManager.getLogger("GetTagDetailsV002");
 
@@ -81,44 +71,35 @@ public class GetTagDetailsV002 extends HttpServlet {
         tagService = appContext.getBean(ITagService.class);
         parameterService = appContext.getBean(IParameterService.class);
         invariantService = appContext.getBean(IInvariantService.class);
-
-        String Tag = ParameterParserUtil.parseStringParam(request.getParameter("Tag"), "");
-        /**
-         * Adding Log entry.
-         */
-        ILogEventService logEventService = appContext.getBean(LogEventService.class);
-        logEventService.createForPublicCalls("/GetTagDetailsV002", "CALL", "GetTagDetails called : " + request.getRequestURL(),
-                request);
-
         apiKeyService = appContext.getBean(IAPIKeyService.class);
         testCaseExecutionService = appContext.getBean(ITestCaseExecutionService.class);
+        logEventService = appContext.getBean(LogEventService.class);
+        logEventService.createForPublicCalls("/GetTagDetailsV002", "CALL", "GetTagDetails called : " + request.getRequestURL(), request);
+
+        String tagParameter = ParameterParserUtil.parseStringParam(request.getParameter("Tag"), "");
+
         if (apiKeyService.authenticate(request, response)) {
-            List<TestCaseExecution> listOfExecutions;
-            List<JSONObject> listOfExecutionsJSON = new ArrayList<>();
             try {
                 // get invariants lists (priorities, countries and env)
-                prioritiesList = invariantService.readByIdName("PRIORITY");
-                countriesList = invariantService.readByIdName("COUNTRY");
-                environmentsList = invariantService.readByIdName("ENVIRONMENT");
-                JSONObject jsonResponse = new JSONObject();
-                Tag tag = tagService.convert(tagService.readByKey(Tag));
+                List<Invariant> prioritiesList = invariantService.readByIdName("PRIORITY");
+                List<Invariant> countriesList = invariantService.readByIdName("COUNTRY");
+                List<Invariant> environmentsList = invariantService.readByIdName("ENVIRONMENT");
 
-                cerberusUrlParameter = parameterService.getParameterStringByKey("cerberus_gui_url", "", "");
+                Tag tag = tagService.convert(tagService.readByKey(tagParameter));
+                String cerberusUrlParameter = parameterService.getParameterStringByKey("cerberus_gui_url", "", "");
                 if (StringUtil.isNullOrEmpty(cerberusUrlParameter)) {
                     cerberusUrlParameter = parameterService.getParameterStringByKey("cerberus_url", "", "");
                 }
 
                 if (tag != null) {
-                    listOfExecutions = testCaseExecutionService.readLastExecutionAndExecutionInQueueByTag(Tag);
+                    List<TestCaseExecution> listOfExecutions = testCaseExecutionService.readLastExecutionAndExecutionInQueueByTag(tag.getTag());
                     tag.setExecutionsNew(listOfExecutions);
-
-                    jsonResponse = tag.toJsonV001(cerberusUrlParameter, prioritiesList, countriesList, environmentsList);
                     response.setContentType("application/json");
-                    response.getWriter().print(jsonResponse.toString());
+                    response.getWriter().print(tag.toJsonV001(cerberusUrlParameter, prioritiesList, countriesList, environmentsList));
                 }
             } catch (CerberusException ex) {
                 LOG.error(ex.getMessageError().getDescription());
-            } catch (ParseException ex) {
+            } catch (ParseException | IOException ex) {
                 LOG.error(ex.getMessage());
             }
         }
