06e169cfe44ad08c22bc6632172ceef175d373ee
MorganLmd <morgan.lombard@protonmail.com>
2022-09-14 16:37:12 +0200

    new AbstractServlet to enable dependency injection in servlets

--- source/src/main/java/org/cerberus/servlet/crud/test/CreateTest.java
+++ source/src/main/java/org/cerberus/servlet/crud/test/CreateTest.java
@@ -19,52 +19,55 @@
  */
 package org.cerberus.servlet.crud.test;
 
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.cerberus.engine.entity.MessageEvent;
 import org.cerberus.crud.entity.Test;
-import org.cerberus.crud.factory.IFactoryLogEvent;
 import org.cerberus.crud.factory.IFactoryTest;
-import org.cerberus.crud.factory.impl.FactoryLogEvent;
 import org.cerberus.crud.service.ILogEventService;
 import org.cerberus.crud.service.ITestService;
-import org.cerberus.crud.service.impl.LogEventService;
+import org.cerberus.engine.entity.MessageEvent;
 import org.cerberus.enums.MessageEventEnum;
+import org.cerberus.servlet.AbstractServlet;
 import org.cerberus.util.ParameterParserUtil;
 import org.cerberus.util.answer.Answer;
 import org.cerberus.util.servlet.ServletUtil;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.owasp.html.PolicyFactory;
-import org.owasp.html.Sanitizers;
-import org.springframework.context.ApplicationContext;
-import org.springframework.web.context.support.WebApplicationContextUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
- *
  * @author cerberus
  */
+@Configurable
 @WebServlet(name = "CreateTest", urlPatterns = {"/CreateTest"})
-public class CreateTest extends HttpServlet {
+public class CreateTest extends AbstractServlet {
 
     private static final Logger LOG = LogManager.getLogger(CreateTest.class);
+    @Autowired
+    private ITestService testService;
+    @Autowired
+    private IFactoryTest factoryTest;
+    @Autowired
+    private ILogEventService logEventService;
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
      *
-     * @param request servlet request
+     * @param request  servlet request
      * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+     * @throws ServletException       if a servlet-specific error occurs
+     * @throws IOException            if an I/O error occurs
      * @throws org.json.JSONException
      */
+    @Override
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException, JSONException {
         JSONObject jsonResponse = new JSONObject();
@@ -72,24 +75,19 @@ public class CreateTest extends HttpServlet {
         MessageEvent msg = new MessageEvent(MessageEventEnum.DATA_OPERATION_ERROR_UNEXPECTED);
         msg.setDescription(msg.getDescription().replace("%DESCRIPTION%", ""));
         ans.setResultMessage(msg);
-        PolicyFactory policy = Sanitizers.FORMATTING.and(Sanitizers.LINKS);
 
         response.setContentType("application/json");
 
         // Calling Servlet Transversal Util.
         ServletUtil.servletStart(request);
 
-        /**
-         * Parsing and securing all required parameters.
-         */
+        // Parsing and securing all required parameters.
         String test = ParameterParserUtil.parseStringParamAndSanitize(request.getParameter("test"), "");
         boolean isActive = ParameterParserUtil.parseBooleanParam(request.getParameter("isActive"), false);
         String parentTest = ParameterParserUtil.parseStringParamAndSanitize(request.getParameter("ParentTest"), null);
         String description = ParameterParserUtil.parseStringParamAndSanitize(request.getParameter("description"), "");
 
-        /**
-         * Checking all constrains before calling the services.
-         */
+        // Checking all constrains before calling the services.
         if (test.isEmpty()) {
             msg = new MessageEvent(MessageEventEnum.DATA_OPERATION_ERROR_EXPECTED);
             msg.setDescription(msg.getDescription().replace("%ITEM%", "Test")
@@ -97,30 +95,16 @@ public class CreateTest extends HttpServlet {
                     .replace("%REASON%", "Test name is missing!"));
             ans.setResultMessage(msg);
         } else {
-            /**
-             * All data seems cleans so we can call the services.
-             */
-            ApplicationContext appContext = WebApplicationContextUtils.getWebApplicationContext(this.getServletContext());
-            ITestService testService = appContext.getBean(ITestService.class);
-            IFactoryTest factoryTest = appContext.getBean(IFactoryTest.class);
 
             Test testData = factoryTest.create(test, description, isActive, parentTest, request.getUserPrincipal().getName(), null, null, null);
             ans = testService.create(testData);
 
             if (ans.isCodeEquals(MessageEventEnum.DATA_OPERATION_OK.getCode())) {
-                /**
-                 * Object created. Adding Log entry.
-                 */
-                ILogEventService logEventService = appContext.getBean(LogEventService.class);
-                IFactoryLogEvent factoryLogEvent = appContext.getBean(FactoryLogEvent.class);
-
                 logEventService.createForPrivateCalls("/CreateTest", "CREATE", "Create Test : ['" + test + "']", request);
             }
         }
 
-        /**
-         * Formating and returning the json result.
-         */
+        // Formatting and returning the json result.
         jsonResponse.put("messageType", ans.getResultMessage().getMessage().getCodeString());
         jsonResponse.put("message", ans.getResultMessage().getDescription());
 
@@ -129,13 +113,14 @@ public class CreateTest extends HttpServlet {
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+
     /**
      * Handles the HTTP <code>GET</code> method.
      *
-     * @param request servlet request
+     * @param request  servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+     * @throws IOException      if an I/O error occurs
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -150,10 +135,10 @@ public class CreateTest extends HttpServlet {
     /**
      * Handles the HTTP <code>POST</code> method.
      *
-     * @param request servlet request
+     * @param request  servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+     * @throws IOException      if an I/O error occurs
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
