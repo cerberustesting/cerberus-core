239535eefcfce8e2f9e3b3fb6e25cbd57b05d327
Benoit DUMONT <bdumont@cerberus-testing.com>
2023-01-29 16:18:25 +0100

    create a new changelog file for the next version

--- source/src/main/webapp/js/pages/Homepage.js
+++ source/src/main/webapp/js/pages/Homepage.js
@@ -129,10 +129,10 @@ $.when($.getScript("js/global/global.js")).then(function () {
         loadBuildRevTable();
 
         // Display Changelog;
-        $("#documentationFrame").attr("src", "./documentation/D2/changelog_4.16_en.html");
+        $("#documentationFrame").attr("src", "./documentation/D2/changelog_4.17_en.html");
         var windowsHeight = $(window).height() + 'px';
         $('#documentationFrame').css('height', '400px');
-        $("#changelogLabel").html("Changelog 4.16");
+        $("#changelogLabel").html("Changelog 4.17");
 
         //close all sidebar menu
         closeEveryNavbarMenu();
