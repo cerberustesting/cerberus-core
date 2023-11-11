f493580750bfbf6efe461b312a71c94c03d4aa6d
Benoit DUMONT <bdumont@cerberus-testing.com>
2023-11-11 16:48:46 +0100

    Fixed Drag and Drop #2036

--- source/src/main/java/org/cerberus/core/service/webdriver/impl/WebDriverService.java
+++ source/src/main/java/org/cerberus/core/service/webdriver/impl/WebDriverService.java
@@ -84,6 +84,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 import java.util.stream.Collectors;
+import org.openqa.selenium.interactions.Action;
 
 /**
  * @author bcivel
@@ -1500,22 +1501,13 @@ public class WebDriverService implements IWebDriverService {
                 WebElement source = (WebElement) answerDrag.getItem();
                 WebElement target = (WebElement) answerDrop.getItem();
                 if (source != null && target != null) {
-                    ((JavascriptExecutor) session.getDriver()).executeScript("function createEvent(typeOfEvent) {\n" + "var event =document.createEvent(\"CustomEvent\");\n"
-                            + "event.initCustomEvent(typeOfEvent,true, true, null);\n" + "event.dataTransfer = {\n" + "data: {},\n"
-                            + "setData: function (key, value) {\n" + "this.data[key] = value;\n" + "},\n"
-                            + "getData: function (key) {\n" + "return this.data[key];\n" + "}\n" + "};\n" + "return event;\n"
-                            + "}\n" + "\n" + "function dispatchEvent(element, event,transferData) {\n"
-                            + "if (transferData !== undefined) {\n" + "event.dataTransfer = transferData;\n" + "}\n"
-                            + "if (element.dispatchEvent) {\n" + "element.dispatchEvent(event);\n"
-                            + "} else if (element.fireEvent) {\n" + "element.fireEvent(\"on\" + event.type, event);\n" + "}\n"
-                            + "}\n" + "\n" + "function simulateHTML5DragAndDrop(element, destination) {\n"
-                            + "var dragStartEvent =createEvent('dragstart');\n" + "dispatchEvent(element, dragStartEvent);\n"
-                            + "var dropEvent = createEvent('drop');\n"
-                            + "dispatchEvent(destination, dropEvent,dragStartEvent.dataTransfer);\n"
-                            + "var dragEndEvent = createEvent('dragend');\n"
-                            + "dispatchEvent(element, dragEndEvent,dropEvent.dataTransfer);\n" + "}\n" + "\n"
-                            + "var source = arguments[0];\n" + "var destination = arguments[1];\n"
-                            + "simulateHTML5DragAndDrop(source,destination);", source, target);
+
+                    Actions builder = new Actions(session.getDriver());
+                    Action dragAndDrop = builder.clickAndHold(source)
+                            .moveToElement(target)
+                            .release(target)
+                            .build();
+                    dragAndDrop.perform();
                 }
                 message = new MessageEvent(MessageEventEnum.ACTION_SUCCESS_DRAGANDDROP);
                 message.setDescription(message.getDescription().replace("%SOURCE%", drag.getIdentifier() + "=" + drag.getLocator()));
