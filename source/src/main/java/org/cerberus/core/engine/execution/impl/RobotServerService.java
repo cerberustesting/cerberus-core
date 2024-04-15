c97266efa30d921a7cb0ea18d78910021145c3f1
MathisSenicourt <mathiscience@hotmail.fr>
2024-04-15 14:45:55 +0200

    identified appium element to fix and remove <p>

--- source/src/main/java/org/cerberus/core/engine/execution/impl/RobotServerService.java
+++ source/src/main/java/org/cerberus/core/engine/execution/impl/RobotServerService.java
@@ -1000,15 +1000,15 @@ public class RobotServerService implements IRobotServerService {
 //                        proxy.setHttpProxy(tCExecution.getRobotExecutorObj().getExecutorProxyHost() + ":" + tCExecution.getRemoteProxyPort());
 //                        proxy.setSslProxy(tCExecution.getRobotExecutorObj().getExecutorProxyHost() + ":" + tCExecution.getRemoteProxyPort());
 //                    }
-//                    capabilities = DesiredCapabilities.android(); //#FIXME SELENIUM
+//                    capabilities = DesiredCapabilities.android(); //#FIXME SELENIUM #APPIUM
                     break;
 
                 case "ipad":
-//                    capabilities = DesiredCapabilities.ipad(); //#FIXME SELENIUM
+//                    capabilities = DesiredCapabilities.ipad(); //#FIXME SELENIUM #APPIUM
                     break;
 
                 case "iphone":
-//                    capabilities = DesiredCapabilities.iphone(); //#FIXME SELENIUM
+//                    capabilities = DesiredCapabilities.iphone(); //#FIXME SELENIUM #APPIUM
                     break;
 
                 case "":
