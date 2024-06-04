19942af6729d55925eb76fcc8c8bb1cd0be4aafa
MathisSenicourt <mathiscience@hotmail.fr>
2024-06-04 10:05:49 +0200

    log trace

--- source/src/main/java/org/cerberus/core/service/webdriver/impl/WebDriverService.java
+++ source/src/main/java/org/cerberus/core/service/webdriver/impl/WebDriverService.java
@@ -866,7 +866,7 @@ public class WebDriverService implements IWebDriverService {
             return message;
 
         } catch (WebDriverException exception) {
-            LOG.warn(exception.toString());
+            LOG.warn(exception.toString(),exception);
             return parseWebDriverException(exception);
         }
 
