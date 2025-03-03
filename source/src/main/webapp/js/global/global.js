52651640d4cc777720657dcfe3241dcccd136e62
lucashimpens <lucas.himpens@orange.fr>
2025-03-03 11:16:57 +0100

    fix: avoid to retrieve the ip of node when robot cloud provider is used.

--- source/src/main/webapp/js/global/global.js
+++ source/src/main/webapp/js/global/global.js
@@ -2389,10 +2389,13 @@ function bindToggleFullscreen() {
     $(".togglefullscreen").unbind("click").click(function () {
         let idTomove = this.getAttribute('for');
         let myButton = document.getElementById(idTomove);
+        console.info(myButton);
         if (myButton.classList.contains("overlay")) {
+            console.info("remove");
             myButton.classList.remove('overlay');
             $(document).unbind("keydown");
         } else {
+            console.info("add");
             myButton.classList.add('overlay');
 
             $(document).bind("keydown", function (e) {
